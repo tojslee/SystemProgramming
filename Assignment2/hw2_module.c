6c00182c01d70494f8f7a79c78e013e5d01b11ba
Lee Jisoo <54274442+tojslee@users.noreply.github.com>
2020-12-10 22:59:39 +0900

    Update hw2_module.c

--- Assignment2/hw2_module.c
+++ Assignment2/hw2_module.c
@@ -8,6 +8,9 @@
 #include <linux/interrupt.h>
 #include <linux/spinlock.h>
 #include <linux/mm.h>
+#include <linux/slab.h>
+#include <linux/uaccess.h>
+#include <linux/kernel.h>
 
 #define DIR_NAME "hw2"
 #define MAX_PID 131072
@@ -27,9 +30,20 @@ DEFINE_SPINLOCK(tasklet_lock);
 char *comm;
 static long page_size = PAGE_SIZE;
 pgd_t* pgd_base;
-struct vm_area_struct *mmap;
+int leng;
 long last_update_time;
-int temp;
+struct vm_area* mmap;
+
+struct vm_area{
+	char read[10];
+	char write[10];
+	char exec[10];
+	char type[30];
+	unsigned long vm_start, vm_end;
+	unsigned long pm_start, pm_end;
+	struct vm_area* next;
+};
+
 
 module_param(period, int, 0);
 module_param(pid, int, 0);
@@ -77,7 +91,7 @@ static int hw2_seq_show(struct seq_file *s, void *v){
 		flag = false;
 	}
 	else{flag = true;}
-
+	spin_lock(&tasklet_lock);
 
 	if(flag){ // valid process
 		print_bar(s);
@@ -89,10 +103,20 @@ static int hw2_seq_show(struct seq_file *s, void *v){
 		seq_printf(s, "Last update time: %llu ms\n", last_update_time);
 		seq_printf(s, "Page Size: %d KB\n", page_size);
 		seq_printf(s, "PGD Base Address: 0x%08lx\n", pgd_base);
-		seq_printf(s, "temp: %d\n", temp);
+		//seq_printf(s, "temp: %d\n", temp);
 		print_bar(s);
 
-		//index = 1;
+		
+		int index = 1;
+		struct vm_area* te = mmap;
+		while(te != NULL){
+			seq_printf(s, "VM Area $%d - %s, (%d pages)\n", index++, te->type, 1);
+			seq_printf(s, "Permissions: R(%s), W(%s), X(%s)\n", te->read, te->write, te->exec);
+			seq_printf(s, "Virtual Memory: 0x%08lx - 0x%08lx\n", te->vm_start, te->vm_end);
+			seq_printf(s, "Physical Memory: 0x%08lx - 0x%08lx\n", te->vm_start, te->vm_end);
+			print_bar(s);
+			te = te->next;
+		}
 
 	}
 	else{ // invlid process
@@ -102,6 +126,7 @@ static int hw2_seq_show(struct seq_file *s, void *v){
 		seq_printf(s, "There is no information - PID: %d\n", pid);
 		print_bar(s);
 	}
+	spin_unlock(&tasklet_lock);
 	
 
 	return 0;
@@ -165,8 +190,93 @@ void tasklet_function(unsigned long data){
 		pgd_base = p->mm->pgd;
 		getnstimeofday(&current_time);
 		last_update_time = current_time.tv_sec*1000+current_time.tv_nsec/1000000;
-		temp = idx++;
-		memcpy(mmap, p->mm->mmap, sizeof(struct vm_area_struct));
+		
+		
+		struct vm_area_struct *fos = p->mm->mmap;
+		struct vm_area *tp;
+		while(mmap != NULL){
+			tp = mmap;
+			mmap = mmap->next;
+			kfree(tp);
+		}
+		mmap = kmalloc(sizeof(struct vm_area), GFP_KERNEL);
+		struct vm_area* cur = mmap;
+		while(fos != NULL){
+			cur->vm_start = fos->vm_start;
+			cur->vm_end = fos->vm_end;
+
+			// area
+			if(p->mm->start_code <= fos->vm_start && p->mm->end_code >= fos->vm_end){
+				// TEXT area
+				//codes[temp] = "Code";
+				strcpy(cur->type, "Code");
+			}
+			else if(p->mm->start_data <= fos->vm_start && p->mm->end_data >= fos->vm_end){
+				// DATA area
+				//codes[temp] = "Data";
+				strcpy(cur->type,"Data");
+			}
+			else if(p->mm->end_data < fos->vm_start && p->mm->start_brk > fos->vm_end){
+				// BSS
+				//codes[temp] = "BSS";
+				strcpy(cur->type,"BSS");
+			}
+			else if(p->mm->start_brk <= fos->vm_start && p->mm->brk >= fos->vm_end){
+				// heap
+				//codes[temp] = "Heap";
+				strcpy(cur->type,"Heap");
+			}
+			else if(p->mm->start_stack >= fos->vm_start && p->mm->start_stack <= fos->vm_end){
+				// stack
+				//codes[temp] = "Stack";
+				strcpy(cur->type,"Stack");
+			}
+			else{
+				//shared
+				//codes[temp] = "Shared Libraries";
+				strcpy(cur->type,"Shared Libraries");
+			}
+
+			// access
+			int flags = p->mm->mmap->vm_flags;
+			if(flags & VM_READ){
+				strcpy(cur->read,"yes");
+			}
+			else{strcpy(cur->read,"no");}
+			if (flags & VM_WRITE){
+				strcpy(cur->write,"yes");
+			}
+			else{strcpy(cur->write,"no");}
+			if(flags & VM_EXEC){
+				strcpy(cur->exec,"yes");
+			}
+			else{strcpy(cur->exec,"no");}
+			//temp++;
+			pgd_t *pgd;
+			pud_t *pud;
+			pmd_t *pmd;
+			pte_t *pte;
+
+			pgd = pgd_offset(p->mm, fos->vm_start);
+			pud = pud_offset(pgd, fos->vm_start);
+			pmd = pmd_offset(pud, fos->vm_start);
+			pte = pte_offset_kernel(pmd, fos->vm_start);
+			//struct page* pag = pte_page(*pte);
+			//cur->pm_start = pag->virtual;
+			cur->pm_start = pte_val(*pte) & PAGE_MASK;
+
+			/*pud = pud_offset(pgd_base, fos->vm_end);
+			pmd = pmd_offset(pud, fos->vm_end);
+			pte = pte_offset_map(pmd, fos->vm_end);
+			pag = pte_page(pte);
+			cur->pm_end = pag->virtual;*/
+
+			fos = fos->vm_next;
+			cur->next = kmalloc(sizeof(struct vm_area), GFP_KERNEL);
+			cur = cur->next;
+		}
+		cur = NULL;
+
 	}
 	else{ // else
 		last_update_time = -1;
@@ -180,12 +290,15 @@ static int __init hw2_init(void){
 	proc_file_entry = proc_create(DIR_NAME, 0, NULL, &hw2_file_ops);
 	printk("%d %d\n", period, pid);
 
+	mmap = kmalloc(sizeof(struct vm_area), GFP_KERNEL);
+
 	// timer initialization
 	timer_setup(&tasklet_timer, tasklet_timer_handler, 0);
 	mod_timer(&tasklet_timer, jiffies + HZ*period);
 
 	//tasklet initialization
 	tasklet_init(&hw2_tasklet, tasklet_function, 0);
+	tasklet_schedule(&hw2_tasklet);
 
 	return 0;
 }
@@ -195,6 +308,12 @@ static void __exit hw2_exit(void){
 	remove_proc_entry(DIR_NAME, NULL);
 	del_timer(&tasklet_timer);
 	tasklet_kill(&hw2_tasklet);
+	struct vm_area* tp;
+	while(mmap != NULL){
+		tp = mmap;
+		mmap = mmap->next;
+		kfree(tp);
+	}
 }
 
 module_init(hw2_init);
